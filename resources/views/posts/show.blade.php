c5e7badb1a8ca67febb6c2e7d7c22d625657296d
towakey <kyosgame@gmail.com>
2025-02-23 23:09:16 +0900

    リプライでのボタン追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -88,14 +88,34 @@
                         <div class="space-y-6">
                             @foreach($post->replies as $reply)
                             <div class="bg-gray-50 p-4 rounded-lg">
-                                <div class="flex items-center space-x-4 mb-2">
-                                    <div class="flex items-center space-x-2">
-                                        @if($reply->user->avatar)
-                                            <img src="{{ $reply->user->avatar }}" alt="{{ $reply->user->name }}" class="w-8 h-8 rounded-full">
-                                        @endif
-                                        <span class="text-gray-600 font-medium">{{ $reply->user->name }}</span>
+                                <div class="flex justify-between items-start">
+                                    <div class="flex items-center space-x-4 mb-2">
+                                        <div class="flex items-center space-x-2">
+                                            @if($reply->user->avatar)
+                                                <img src="{{ $reply->user->avatar }}" alt="{{ $reply->user->name }}" class="w-8 h-8 rounded-full">
+                                            @endif
+                                            <span class="text-gray-600 font-medium">{{ $reply->user->name }}</span>
+                                        </div>
+                                        <span class="text-gray-500">{{ $reply->created_at->format('Y/m/d H:i') }}</span>
                                     </div>
-                                    <span class="text-gray-500">{{ $reply->created_at->format('Y/m/d H:i') }}</span>
+                                    @can('update', $reply)
+                                    <div class="flex space-x-2">
+                                        <a href="{{ route('posts.edit', $reply) }}" class="text-yellow-500 hover:text-yellow-700">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                                            </svg>
+                                        </a>
+                                        <form method="POST" action="{{ route('posts.destroy', $reply) }}" class="inline" onsubmit="return confirm('本当に削除しますか？');">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="text-red-500 hover:text-red-700">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                </svg>
+                                            </button>
+                                        </form>
+                                    </div>
+                                    @endcan
                                 </div>
                                 <h4 class="font-semibold mb-2">{{ $reply->title }}</h4>
                                 <div class="prose max-w-none">
